fb4b2eeccbd3aa94df46991fb0ca315f3a6088ef
bagas-na <bagasnurseno@gmail.com>
2024-12-18 00:38:45 +0700

    complete exercise 1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
 
   const average = good - bad;
   const count = good + neutral + bad;
-  const positive = count > 0 ? (good / count) + "%" : "-";
+  const positive = count > 0 ? good / count + "%" : "-";
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
@@ -16,14 +32,9 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Average: {average}</p>
-      <o>Positive: {positive}</o>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
