35d55c7ebbc3097a253d25956ff9eadea4e68fde
bagas-na <bagasnurseno@gmail.com>
2024-12-18 23:29:01 +0700

    complete exercise 2.1: course information step 6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,49 +1,66 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
 
-const Part = ({ part }) => 
+const Part = ({ part }) => (
   <p>
     {part.name} {part.exercises}
   </p>
+);
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) => (
   <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
+    {parts.map((part) => (
+      <Part part={part} key={part.id} />
+    ))}
   </>
+);
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+const Course = ({ course }) => {
+  const { name, parts } = course;
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={name} />
       <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+      {
+        name: "Form validation using zod",
+        exercises: 7,
+        id: 5,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
