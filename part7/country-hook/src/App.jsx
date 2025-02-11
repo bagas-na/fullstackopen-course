556022df3cb4f9263c8d62a9e46476e8790e058e
bagas-na <bagasnurseno@gmail.com>
2025-02-11 07:06:17 +0700

    complete exercise 7.7

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -1,60 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState(null);
+  const baseUrl = "https://studies.cs.helsinki.fi/restcountries";
+  const fetchUrl = `${baseUrl}/api/name/${name}`;
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios
+      .get(fetchUrl)
+      .then((response) => {
+        setCountry({ ...response, found: true });
+        console.log(response)
+      })
+      .catch((error) => {
+        setCountry({ ...error.response, found: false })
+        console.error(error.code, error.response.data);
+      });
+  }, [name]);
 
-  return country
-}
+  return country;
+};
 
 const Country = ({ country }) => {
   if (!country) {
-    return null
+    return null;
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>;
   }
+
+  const formatPopulation = new Intl.NumberFormat(undefined, {style: 'decimal'}).format(country.data.population)
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {formatPopulation}</div>
+      <img
+        style={{ border: "1px solid black" }}
+        src={country.data.flags.png}
+        height="100"
+        alt={`flag of ${country.data.name.official}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("indonesia");
+  const country = useCountry(name);
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
   return (
     <div>
@@ -65,7 +81,7 @@ const App = () => {
 
       <Country country={country} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
