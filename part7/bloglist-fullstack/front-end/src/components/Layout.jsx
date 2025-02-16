3ca15020508ed6188d55790293701544a6e234c9
bagas-na <bagasnurseno@gmail.com>
2025-02-17 00:29:33 +0700

    refactor and style navbar

--- part7/bloglist-fullstack/front-end/src/components/Layout.jsx
+++ part7/bloglist-fullstack/front-end/src/components/Layout.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { initializeBlogs } from '../reducers/blogReducer'
-import { logoutSession } from '../reducers/sessionReducer'
 import { initializeUsers } from '../reducers/userReducer'
 import BlogForm from './BlogForm'
 import Navbar from './Navbar'
@@ -24,19 +23,12 @@ const Layout = () => {
     }
   }, [navigate, session.token])
 
-  const logoutHandler = () => {
-    dispatch(logoutSession())
-    navigate('/login')
-  }
-
   return (
     <div>
       <Navbar/>
       <h2>Blog app</h2>
       <Notification />
       <BlogForm session={session} />
-      <p>{session.name} logged in.</p>
-      <button onClick={() => logoutHandler()}>log out</button>
       <Outlet />
     </div>
   )
--- part7/bloglist-fullstack/front-end/src/components/Navbar.jsx
+++ part7/bloglist-fullstack/front-end/src/components/Navbar.jsx
@@ -1,16 +1,73 @@
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { logoutSession } from '../reducers/sessionReducer'
 
 const Navbar = () => {
+  const session = useSelector(({ session }) => session)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const logoutHandler = () => {
+    dispatch(logoutSession())
+    navigate('/login')
+  }
+
   return (
-    <nav>
-      <ol style={{ margin: 0, padding: 0 }}>
-        <li style={{ display: 'inline', listStyle: 'none', marginRight: '0.5rem' }}>
-          <Link to='/'>blogs</Link>
+    <nav
+      style={{
+        margin: '-0.5rem',
+        padding: '0.5rem',
+        backgroundColor: 'lightgray',
+      }}
+    >
+      <ol style={{ display: 'inline-block', margin: 0, padding: 0 }}>
+        <li
+          style={{
+            display: 'inline',
+            listStyle: 'none',
+            marginRight: '0.5rem',
+          }}
+        >
+          <NavLink
+            to='/'
+            style={({ isActive }) => ({
+              fontWeight: isActive ? 'bold' : 'normal',
+            })}
+          >
+            blogs
+          </NavLink>
         </li>
-        <li style={{ display: 'inline', listStyle: 'none', marginRight: '0.5rem' }}>
-          <Link to='/users'>users</Link>
+        <li
+          style={{
+            display: 'inline',
+            listStyle: 'none',
+            marginRight: '0.5rem',
+          }}
+        >
+          <NavLink
+            to='/users'
+            style={({ isActive }) => ({
+              fontWeight: isActive ? 'bold' : 'normal',
+            })}
+          >
+            users
+          </NavLink>
         </li>
       </ol>
+      <div style={{ display: 'inline' }}>
+        <p
+          style={{
+            display: 'inline',
+            marginLeft: '0.5rem',
+            marginRight: '0.5rem',
+          }}
+        >
+          {session.name} logged in.
+        </p>
+        <button style={{ display: 'inline' }} onClick={() => logoutHandler()}>
+          log out
+        </button>
+      </div>
     </nav>
   )
 }
