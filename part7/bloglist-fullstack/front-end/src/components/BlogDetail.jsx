a1980486aa44358a8193bb26b9601eeee76f4755
bagas-na <bagasnurseno@gmail.com>
2025-02-18 22:30:36 +0700

    refactor react component to reduce nesting

--- part7/bloglist-fullstack/front-end/src/components/BlogDetail.jsx
+++ part7/bloglist-fullstack/front-end/src/components/BlogDetail.jsx
@@ -1,6 +1,51 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
-import { incrementLike } from '../reducers/blogReducer'
+import { addCommentOfBlog, incrementLike } from '../reducers/blogReducer'
+
+const CommentList = ({ comments }) => {
+  return (
+    <ul style={{ margin: 0 }}>
+      {comments.map((comment) => (
+        <li key={comment}>{comment}</li>
+      ))}
+    </ul>
+  )
+}
+CommentList.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.string.isRequired),
+}
+
+const CommentForm = () => {
+  const params = useParams()
+  const blog = useSelector(({ blogs }) =>
+    blogs.find((blog) => blog.id === params.id)
+  )
+  const [comment, setComment] = useState('')
+  const dispatch = useDispatch()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    dispatch(addCommentOfBlog(blog, comment))
+    setComment('')
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type='text'
+        name='comment'
+        id='comment'
+        placeholder='write your comment here...'
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button type='submit'>add comment</button>
+    </form>
+  )
+}
 
 const BlogDetail = () => {
   const params = useParams()
@@ -26,14 +71,21 @@ const BlogDetail = () => {
         <p style={{ margin: 0 }}>{blog.url}</p>
         <div>
           <p style={{ margin: 0, display: 'inline' }}>{blog.likes} likes</p>
-          <button style={{ margin: 6 }} onClick={likeHandler}>like</button>
+          <button style={{ margin: 6 }} onClick={likeHandler}>
+            like
+          </button>
         </div>
-        <p style={{ margin: 0 }}>added by <Link to={`/users/${blog.user.id}`} >{blog.user.name}</Link></p>
+        <p style={{ margin: 0 }}>
+          added by <Link to={`/users/${blog.user.id}`}>{blog.user.name}</Link>
+        </p>
       </div>
       <h3 style={{ marginBottom: '0.5rem' }}>comments:</h3>
+      <CommentForm />
       {blog.comments.length > 0 ? (
-        <ul style={{ margin: 0 }}>{blog.comments.map(comment => <li key={comment}>{comment}</li>)}</ul>
-      ): <p>No comments has been added</p>}
+        <CommentList comments={blog.comments} />
+      ) : (
+        <p>No comments has been added</p>
+      )}
     </div>
   )
 }
--- part7/bloglist-fullstack/front-end/src/components/UserDetail.jsx
+++ part7/bloglist-fullstack/front-end/src/components/UserDetail.jsx
@@ -1,5 +1,30 @@
+import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
+
+const BlogList = ({ blogs }) => {
+  return (
+    <ul>
+      {blogs.map((blog) => (
+        <li key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
+            {blog.title}
+            {blog.author && ` -- ${blog.author}`}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+BlogList.propTypes = {
+  blogs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string,
+    })
+  ),
+}
 
 const UserDetail = () => {
   const params = useParams()
@@ -15,20 +40,10 @@ const UserDetail = () => {
     <div>
       <h2>{user.name}</h2>
       <h3>added blogs:</h3>
-      {user.blogs.length > 0 ? (
-        <ul>
-          {user.blogs.map((blog) => (
-            <li key={blog.id}>
-              <Link to={`/blogs/${blog.id}`}>
-                {blog.title}
-                {blog.author && ` -- ${blog.author}`}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>This user has not added any blogs</p>
-      )}
+      {user.blogs.length > 0
+        ? <BlogList blogs={user.blogs} />
+        :<p>This user has not added any blogs</p>
+      }
     </div>
   )
 }
