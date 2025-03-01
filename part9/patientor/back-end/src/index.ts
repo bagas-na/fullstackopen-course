2c0483ff1bef192aa4667c7e9e2cf71f234de716
bagas-na <bagasnurseno@gmail.com>
2025-03-01 20:01:01 +0700

    complete exercise 9.9. Installed cors in the backend to resolve CORS related errors

--- part9/patientor/back-end/src/index.ts
+++ part9/patientor/back-end/src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+import cors from "cors";
+import express from "express";
+
 const app = express();
 app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+app.get("/api/ping", cors(), (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
 });
 
 app.listen(PORT, () => {
